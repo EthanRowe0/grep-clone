bdaa2d3a4887f11e6909faf9e176df965f5319f4
Ethan Rowe <roweelg@gmail.com>
2025-08-16 17:27:12 -0400

    added whole word flag

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,20 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[command(name = "grepclone")]
+#[command(about = "A simple grep clone")]
 pub struct Config {
     pub query: String,
     pub filename: String,
-}
 
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments! Try running 'cargo run (search_text) (file)' instead.");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
+    #[arg(long)]
+    pub ignore_case: bool,
+    
+    #[arg(long)]
+    pub whole_word:bool,
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
@@ -25,8 +23,78 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
 
-    println!("With text:\n{}", contents);
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents, config.whole_word)
+    } else {
+        search(&config.query, &contents, config.whole_word)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
 
     Ok(())
 }
 
+pub fn search<'a>(query: &str, contents: &'a str, whole_word: bool) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| {
+            if whole_word {
+                line.split_whitespace().any(|word| word == query)
+            } else {
+                line.contains(query)
+            }
+        })
+        .collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str, whole_word: bool) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    contents
+        .lines()
+        .filter(|line| {
+            if whole_word {
+                line.split_whitespace().any(|word| word.to_lowercase() == query)
+            } else {
+                line.to_lowercase().contains(&query)
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "exist";
+        let contents = "/
+Some multiline
+text that
+should exist.";
+
+        assert_eq!(
+            vec!["should exist."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "IN";
+        let contents = "/
+SOME strange
+CASES iN thiS
+muLTILinE tExt
+that sHouLD
+exist.";
+
+        assert_eq!(
+            vec!["CASES iN thiS", "muLTILinE tExt"],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
 extern crate grepclone;
 
-use std::env;
 use std::process;
-
-use grepclone::Config;
+use clap::Parser;
+use grepclone::{Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let config = Config::parse();
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem passing argumens: {}", err);
-        process::exit(1);
-    });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-    
-    if let Err(e) = grepclone::run(config) {
-        println!("Application error: {}", e);
-
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
-
