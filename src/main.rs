39f874e9b247e951ca899dfede94c66eec729826
Ethan Rowe <roweelg@gmail.com>
2025-08-16 15:43:57 -0400

    created lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,25 @@
+extern crate grepclone;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+use grepclone::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem passing argumens: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
+    
+    if let Err(e) = grepclone::run(config) {
+        println!("Application error: {}", e);
 
-    let mut f = File::open(config.filename).expect("file not found");
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Config { query, filename }
+        process::exit(1);
     }
 }
+
